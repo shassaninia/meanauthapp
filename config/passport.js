e3930170cc4266e25fb660a2767d27a919dd8357
shassaninia <xai.soft80@gmail.com>
2017-02-21 17:07:30 -0500

    passport configuartion

---
+++ config/passport.js
@@ -0,0 +1,25 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
+const config = require('./database');
+
+module.exports = function(passport){
+    let options = {};
+    options.jwtFromRequest = ExtractJwt.fromAuthHeader();
+    options.secretOrKey = config.secret;
+    passport.use(new JwtStrategy(options,(jwt_payload, done) =>{
+        User.getUserById(jwt_payload._id,(error, user)=> {
+            if(error)
+            {
+            return done(error,false);
+            }
+            if(user){
+                return done(null,user);
+                
+            }
+            else{
+                return done(null,false);
+            }
+        })
+    }));
+}
