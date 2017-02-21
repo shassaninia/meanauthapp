d7da6134a469d432e765f5d59110d2e0eb3b1590
shassaninia <xai.soft80@gmail.com>
2017-02-21 12:40:34 -0500

    User Schema and Get User functions

---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const bcrypt  = require('bcryptjs');
+const config = require('../config/database');
+
+//User Schema for user document
+const UserSchema = mongoose.Schema({
+    name:{
+        type:String
+    },
+    emai:{
+        type:String,
+        required:true
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+});
+
+const User = module.exports = mongoose.model('User',UserSchema);
+
+module.exports.getUserById = function(id, callback){
+    User.findById(id,callback);
+}
+
+
+module.exports.getUserByUsername = function(username, callback){
+    const query = {username:username};
+    User.findOne(query,callback);
+}
