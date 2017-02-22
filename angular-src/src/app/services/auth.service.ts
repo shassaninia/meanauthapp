24cbd03bc2241276e61ae2cf11a6144203f1e891
shassaninia <xai.soft80@gmail.com>
2017-02-22 16:24:44 -0500

    Login functionality

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -19,4 +19,25 @@ export class AuthService {
     return this.http.post('http://localhost:3000/users/register',user,{headers:headers})
                .map(response =>response.json());
   }
+
+  authenticateUser(user){
+
+    let headers = new Headers();
+
+    headers.append('content-type','application/json');
+
+    return this.http.post('http://localhost:3000/users/authenticate',user,{headers:headers})
+               .map(response =>response.json());
+  }
+
+  storeUserData(token, user){
+    //passport jwt will automatically look for id_token by default
+    localStorage.setItem('id_token',token);
+
+    //local storage only stores strings, so we stringify our user object
+    localStorage.setItem('user',JSON.stringify(user));
+
+    this.authToken = token;
+    this.user = user;
+  }
 }
