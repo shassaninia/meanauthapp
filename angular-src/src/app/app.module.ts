2cbdaf977bc214156ee55f8aa54e6e3e30d73abd
shassaninia <xai.soft80@gmail.com>
2017-02-22 15:34:09 -0500

    implemented flash messages

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
 import {ValidateService} from './services/validate.service';
+import {FlashMessagesModule} from 'angular2-flash-messages';
 
 const appRoutes: Routes = [
   {
@@ -50,7 +51,8 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule
   ],
   providers: [ValidateService],
   bootstrap: [AppComponent]
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ValidateService} from '../../services/validate.service';
+import {FlashMessagesService} from 'angular2-flash-messages';
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -11,7 +13,8 @@ export class RegisterComponent implements OnInit {
   email:String;
   password:String;
   
-  constructor(private validateService:ValidateService) { }
+  constructor(private validateService:ValidateService,
+              private flashMessagesService: FlashMessagesService) { }
 
   ngOnInit() {
   }
@@ -28,12 +31,14 @@ export class RegisterComponent implements OnInit {
     //Required Fields
     if(!this.validateService.validateRegister(user))
     {
+      this.flashMessagesService.show('Please fill in all the fields',{cssClass:'alert-danger',timeout:3000});
       return false;
     }
 
     //Validate email
     if(!this.validateService.validateEmail(user.email))
     {
+      this.flashMessagesService.show('Please use a valid email',{cssClass:'alert-danger',timeout:3000});
       return false;
     }
 
