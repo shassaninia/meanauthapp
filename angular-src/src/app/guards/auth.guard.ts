4bf61b1ad9c9a30db28c38b50fa352a300a7d883
shassaninia <xai.soft80@gmail.com>
2017-02-22 17:09:34 -0500

    implement canActivate

---
+++ angular-src/src/app/guards/auth.guard.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import {Router,CanActivate} from '@angular/router';
+import {AuthService} from '../services/auth.service';
+
+@Injectable()
+export class AuthGuard implements CanActivate{
+
+    constructor(private authSerive:AuthService,
+                private router:Router){
+
+    }
+
+    canActivate(){
+        if(this.authSerive.loggedIn()){
+            return true;
+        }
+        else{
+            this.router.navigate(['/login']);
+            return false;
+        }
+    }
+}
